c0170399e5407aad16d57c71dd4062f97423b1ea
Benhin <benhinmwendwa7@gmail.com>
2024-04-12 11:10:21 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const testVar = "hi"
+
+
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "ben is the champion!";
+newHeader.className = "ben";
+newHeader.className = "ben";
